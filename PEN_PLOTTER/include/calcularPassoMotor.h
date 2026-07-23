46e2d05efdfbc87214e1ab2895a35d3551d61a3f
Éric Gomes <100045155+ericGL26@users.noreply.github.com>
2026-07-23 17:49:21 -0300

    Delete PEN_PLOTTER directory

--- PEN_PLOTTER/include/calcularPassoMotor.h
+++
@@ -1,9 +0,0 @@
-#ifndef CALCULAR_PASSOS_MOTOR_H
-#define CALCULAR_PASSOS_MOTOR_H
-
-#include <Arduino.h>
-#include "lerTratarDadosUGS.h"
-
-int calcularPassoMotorX(const GCodeStruct& structGCode);
-int calcularPassoMotorY(const GCodeStruct& structGCode);
-#endif
